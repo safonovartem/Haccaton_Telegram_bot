894104fabf8525a5c8059ccdfb5237c31a8891da
safonovartem <safonovartem829@gmail.com>
2023-09-30 13:17:24 +0300

    Change project

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def start(message):
     bot.send_message(message.chat.id, "Что вы хотите сделать?\n"
                                       "/start - Активировать бота\n"
                                       "/find - Найти то, что вам нужно\n"
-                                      "/complain - Пожаловаться на работу бота или сотрудников нашей компании", reply_markup=markup)
+                                      "/complain - Пожаловаться на работу бота или сотрудников", reply_markup=markup)
     bot.send_message(message.chat.id, "Выберите команду", reply_markup=markup)
 
 @bot.message_handler(commands = ['find'])
@@ -50,12 +50,42 @@ def collaborator(message):
     markup.add(types.InlineKeyboardButton("Посмотреть список сотрудников", url="https://kvantorium69.ru/%d0%be-%d0%bd%d0%b0%d1%81/"))  # Текст Кнопки и адрес ссылки
     bot.send_message(message.chat.id, "Список сотрудников доступен на сайте",reply_markup=markup)
 
+@bot.message_handler(commands = ['departments'])
+def departments(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    bot.send_message(message.chat.id, "IT", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее", url="https://kvantorium69.ru/it-%d0%ba%d0%b2%d0%b0%d0%bd%d1%82%d1%83%d0%bc/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "VR/AR", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/vr-ar/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "Аэроквантум", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/%d0%b0%d1%8d%d1%80%d0%be%d0%ba%d0%b2%d0%b0%d0%bd%d1%82%d1%83%d0%bc/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "Промробоквантум", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/%d0%bf%d1%80%d0%be%d0%bc%d1%80%d0%be%d0%b1%d0%be%d0%ba%d0%b2%d0%b0%d0%bd%d1%82%d1%83%d0%bc/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "Промышленный дизайн", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/%d0%bf%d1%80%d0%be%d0%bc%d1%8b%d1%88%d0%bb%d0%b5%d0%bd%d0%bd%d1%8b%d0%b9-%d0%b4%d0%b8%d0%b7%d0%b0%d0%b9%d0%bd/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "Хайтек", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/%d1%85%d0%b0%d0%b9-%d1%82%d0%b5%d0%ba/"))  # Текст Кнопки и адрес ссылки
+
+    bot.send_message(message.chat.id, "Медиа", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("Узнать подробнее",
+                                          url="https://kvantorium69.ru/media/"))  # Текст Кнопки и адрес ссылки
+
 @bot.message_handler(commands = ['complain'])
 def complain(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width= 2)#Параметры: подстраиваться под размеры = Да, Сколько кнопок в ряде
-    Collaborator = types.KeyboardButton("/Collaborator")
-    Bot = types.KeyboardButton("/Bot")
-    Company_management = types.KeyboardButton("/Сompany_management")
+    Collaborator = types.KeyboardButton("/collaborator")
+    Bot = types.KeyboardButton("/cot")
+    Company_management = types.KeyboardButton("/company_management")
     markup.add(Collaborator, Bot, Company_management)#Текст Кнопки и адрес ссылки
     bot.send_message(message.chat.id, "На что вы хотите пожаловаться?", reply_markup=markup)
 
